c3434d00e224f73bbea9925dca680487552e6671
Herman Ras <hermanr@PetraGroup.local>
2020-03-30 16:29:16 +0200

    Added health bar

--- js/obj/frog.js
+++ js/obj/frog.js
@@ -2,6 +2,18 @@ class Frog {
 
     loadAssets() {
         this.img = loadImage('img/froggy.gif');
+        this.lifeImg = new Array();
+        this.lifeImg[0] = loadImage('img/life/VIDA_0.png');
+        this.lifeImg[1] = loadImage('img/life/VIDA_1.png');
+        this.lifeImg[2] = loadImage('img/life/VIDA_2.png');
+        this.lifeImg[3] = loadImage('img/life/VIDA_3.png');
+        this.lifeImg[4] = loadImage('img/life/VIDA_4.png');
+        this.lifeImg[5] = loadImage('img/life/VIDA_5.png');
+        this.lifeImg[6] = loadImage('img/life/VIDA_6.png');
+        this.lifeImg[7] = loadImage('img/life/VIDA_7.png');
+        this.lifeImg[8] = loadImage('img/life/VIDA_8.png');
+        this.lifeImg[9] = loadImage('img/life/VIDA_9.png');
+        this.lifeImg[10] = loadImage('img/life/VIDA_10.png');
     }
 
     constructor() {
@@ -38,5 +50,10 @@ class Frog {
     show() {
         this.move();
         image(this.img, this.posX, this.posY, this.size, this.size);
+        let lifeID = floor(this.health / 10);
+        console.log(lifeID);
+
+        image(this.lifeImg[lifeID], gameW - 110, 10, 100, 10);
+
     }
 }
